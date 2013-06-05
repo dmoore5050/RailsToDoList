ac45612ded4322bfe7b3ee5cb184475d3e8c278b
dmoore5050 <dustinmoore@gmail.com>
2013-06-05 11:51:01 -0500

    removed fixtures/added capybara test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,15 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
+require 'capybara/rails'
+#Capybara.default_driver = :selenium
+
 class ActiveSupport::TestCase
 
+end
+
+class ActionDispatch::IntegrationTest
+  # Make the Capybara DSL available in all integration tests
+  include Capybara::DSL
+  self.use_transactional_fixtures = false
 end
